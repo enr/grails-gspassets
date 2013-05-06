e3daf4217407febc78e6fa166b324b9337d248e8
enr <enrico.comiti@gmail.com>
2013-05-06 10:16:13 +0200

    cleanup BuildConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,4 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = 'target'
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
@@ -17,11 +15,11 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":release:2.0.4",
-              ":rest-client-builder:1.0.2") {
+        build(':release:2.2.0', ':rest-client-builder:1.0.3') {
             export = false
         }
         test(":spock:0.7") {
+            export = false
             exclude "spock-grails-support"
         }
     }
